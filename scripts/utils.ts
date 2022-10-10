e8cafe54a97e8fe57c3bbe34a1c8e3776c96176b
Daniel J. Antonio <djedidiaha@gmail.com>
2022-10-10 21:35:54 +0700

    add comments, fix import

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,9 +1,12 @@
-import { ethers } from 'hardhat';
+import 'hardhat';
+import { ethers } from 'ethers';
 
 const main = async () => {
+	// Set up providers
 	const localProviderUrl = 'http://127.0.0.1:8545/';
 	const provider = new ethers.providers.JsonRpcProvider(localProviderUrl);
 
+	// Get and log the account 0 balance
 	const account0Address = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
 	const account0Balance = await provider.getBalance(account0Address);
 	console.log('account0Balance', ethers.utils.formatEther(account0Balance));
