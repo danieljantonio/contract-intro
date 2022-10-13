d0128812e9482e21ee237b9f193953041c0fef45
Daniel J. Antonio <djedidiaha@gmail.com>
2022-10-13 12:24:56 +0700

    hello world contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,36 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat';
+
+async function deployLock() {
+	const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+	const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+	const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+
+	const lockedAmount = ethers.utils.parseEther('1');
+
+	const Lock = await ethers.getContractFactory('Lock');
+	const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+	await lock.deployed();
+
+	console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+}
+
+async function deployGreeter() {
+	const Greeter = await ethers.getContractFactory('Greeter');
+	const greeter = await Greeter.deploy('Hello, Hardhat!');
+
+	await greeter.deployed();
+
+	console.log('Greeter deployed to:', greeter.address);
+}
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+	deployGreeter();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+	console.error(error);
+	process.exitCode = 1;
 });
