39a7e49b44f0e70fce30222ce7484446133f2d49
Daniel J. Antonio <djedidiaha@gmail.com>
2022-10-18 13:44:38 +0700

    add configs for alchemy api

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,16 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import 'dotenv/config';
 
 const config: HardhatUserConfig = {
 	solidity: '0.8.17',
 	defaultNetwork: 'localhost',
+	networks: {
+		goerli: {
+			url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
+			accounts: [`${process.env.WALLET_PRIVATE_KEY}`],
+		},
+	},
 };
 
 export default config;
